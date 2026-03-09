29e1335e1aa5c43a56c926e1a163f3c0b4fb3602
Henrique <hberdoldifiorotti@gmail.com>
2026-03-08 23:34:22 -0300

    feat: update header styles for improved visual consistency

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,15 +1,17 @@
+
+
 export default function Header(){
     return(
         <>
-        <div className="fixed main-header flex justify-between items-center pl-[15%] pr-[15%] w-[100%] h-16 bg-[#d9d9d9] border-2 border-[#2222222a]">
+        <div className="fixed main-header flex justify-between items-center pl-[15%] pr-[15%] w-[100%] h-16 bg-[#ffffff] border-2 border-[#2222222a]">
             <a className="icon-header h-[100%] w-8">
                 <img src="/Orbis.svg" alt="" className="h-[100%] w-[100%] cursor-[pointer]" />
             </a>
             <div className="actions-header flex gap-x-9.5 justify-center items-center text-[10pt]">
-            <span className="text-black">Sobre</span>
-            <span className="text-black">Contato</span>
+            <span className="text-[#2e2e2e] font-[var(--font-poppins)]">Sobre</span>
+            <span className="text-[#2e2e2e]">Contato</span>
 
-            <a href="" className="border-2 border-fuchsia-600 text-fuchsia-600 p-2 w-[100px] flex justify-center rounded-2xl cursor-pointer">Login</a>
+            <a href="" className="border-2 border-[#5e17eb] text-[#5e17eb] p-1.5 w-[100px] flex justify-center rounded-[10px] cursor-pointer">Login</a>
             </div>
         </div>
         </>
