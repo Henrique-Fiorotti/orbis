391b7dd09032a5121e6ae128ea4c247243517551
Henrique-Fiorotti <hberdoldifiorotti@gmail.com>
2026-03-09 11:54:41 -0300

    Refactor code structure for improved readability and maintainability

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,7 +1,16 @@
 export default function notfound(){
     return(
         <>
-            <h1>Página Erro 404</h1>
+            <div className="w-full h-svh bg-white flex justify-center items-center">
+                <div className="flex flex-col justify-center items-center gap-y-5">
+                    <img src="/hero_404.svg" alt="" />
+                    <h1 className="text-6xl text-[#5e17eb] font-light">404</h1>
+                    <div className="text-center">
+                        <h3 className="text-black">Parece que você se perdeu</h3>
+                        <h3 className="text-black">Voltar para <a href="" className="text-[#5e17eb] underline">Página inicial</a></h3>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
