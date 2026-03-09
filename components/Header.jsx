df4f349c973e4c52830cdae0cb1c7d205127227c
Henrique-Fiorotti <hberdoldifiorotti@gmail.com>
2026-03-09 08:45:31 -0300

    feat: update header links to use anchor tags for improved accessibility and styling

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,14 +4,14 @@ export default function Header(){
     return(
         <>
         <div className="fixed main-header flex justify-between items-center pl-[15%] pr-[15%] w-[100%] h-16 bg-[#ffffff] border-2 border-[#2222222a]">
-            <a className="icon-header h-[100%] w-8">
+            <a href="" className="icon-header h-[100%] w-8" >
                 <img src="/Orbis.svg" alt="" className="h-[100%] w-[100%] cursor-[pointer]" />
             </a>
             <div className="actions-header flex gap-x-9.5 justify-center items-center text-[10pt]">
-            <span className="text-[#2e2e2e] font-[var(--font-poppins)]">Sobre</span>
-            <span className="text-[#2e2e2e]">Contato</span>
+            <a href="" className="text-[#2e2e2e] font-(--font-poppins) hover:text-[#5e17eb]">Sobre</a>
+            <a href="" className="text-[#2e2e2e] font-(--font-poppins) hover:text-[#5e17eb]">Contato</a>
 
-            <a href="" className="border-2 border-[#5e17eb] text-[#5e17eb] p-1.5 w-[100px] flex justify-center rounded-[10px] cursor-pointer">Login</a>
+            <a href="" className="border-2 border-[#5e17eb] text-[#5e17eb] p-1.5 w-[90px] flex justify-center rounded-[10px] cursor-pointer font-(--font-poppins) hover:bg-[#5e17eb2a]   hover:transition-colors">Login</a>
             </div>
         </div>
         </>
