6f7b4aabe574cad0e0aefc27db85790017046cdf
Henrique-Fiorotti <hberdoldifiorotti@gmail.com>
2026-03-09 11:41:49 -0300

    feat: enhance header component with improved styling and layout consistency

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,15 +3,15 @@
 export default function Header(){
     return(
         <>
-        <div className="fixed main-header flex justify-between items-center pl-[15%] pr-[15%] w-[100%] h-16 bg-[#ffffff] border-2 border-[#2222222a]">
-            <a href="" className="icon-header h-[100%] w-8" >
+        <div className="fixed main-header flex justify-between items-center pl-[15%] pr-[15%] w-full h-16 bg-[#ffffff] border-2 border-[#2222222a]">
+            <a href="" className="icon-header h-full w-8" >
                 <img src="/Orbis.svg" alt="" className="h-[100%] w-[100%] cursor-[pointer]" />
             </a>
             <div className="actions-header flex gap-x-9.5 justify-center items-center text-[10pt]">
-            <a href="" className="text-[#2e2e2e] font-(--font-poppins) hover:text-[#5e17eb]">Sobre</a>
-            <a href="" className="text-[#2e2e2e] font-(--font-poppins) hover:text-[#5e17eb]">Contato</a>
+            <a href="" className="relative inline-block text-[#2e2e2e] font-poppins hover:text-[#5e17eb] hover:-top-0.5 transition-all duration-150 ease-in-out">Sobre</a>
+            <a href="" className="relative inline-block text-[#2e2e2e] font-poppins hover:text-[#5e17eb] hover:-top-0.5 transition-all duration-150 ease-in-out">Contato</a>
 
-            <a href="" className="border-2 border-[#5e17eb] text-[#5e17eb] p-1.5 w-[90px] flex justify-center rounded-[10px] cursor-pointer font-(--font-poppins) hover:bg-[#5e17eb2a]   hover:transition-colors">Login</a>
+            <a href="" className="border-2 border-[#5e17eb] text-[#5e17eb] p-1.5 w-22.5 flex justify-center rounded-[10px] cursor-pointer font-poppins hover:bg-[#5013ca] hover:text-[#ffffff] hover:border-[#5013ca] transition-all duration-150 ease-in-out">Login</a>
             </div>
         </div>
         </>
