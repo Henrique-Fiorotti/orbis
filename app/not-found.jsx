9f43bf200945b9b3244e2b98eeff5d09e64c93a9
Henrique-Fiorotti <hberdoldifiorotti@gmail.com>
2026-03-17 11:38:49 -0300

    Adjust 404 page consistency

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,13 +1,13 @@
 export default function notfound(){
     return(
         <>
-            <div className="w-full h-svh bg-white flex justify-center items-center">
+            <div className="w-full h-svh bg-white flex justify-center items-center pt-20">
                 <div className="flex flex-col justify-center items-center gap-y-5">
                     <img src="/hero_404.svg" alt="" className="mr-16" />
-                    <h1 style={{fontFamily: "'Poppins', sans-serif", fontSize: "3.5rem", color: "#5e17eb"}} className="text-7xl">404</h1>
-                    <div className="text-center">
-                        <h3 className="text-black text-[15px]!">Parece que você se perdeu</h3>
-                        <h3 className="text-black text-[15px]!">Voltar para <a href="/" className="text-[#5e17eb]! underline!">Página inicial</a></h3>
+                    <h1 className="text-7xl! font-[poppins]! font-[100]! text-[#5e17eb]!">404</h1>
+                    <div className="text-center text-[15px]/5!">
+                        <h3 className="text-black text-[15px]/4! font-[poppins]! font-[100]! font-light!">Parece que você se perdeu</h3>
+                        <h3 className="text-black text-[15px]/1! font-[poppins]! font-light!">Voltar para <a href="/" className="text-[#5e17eb]! underline!">Página inicial</a></h3>
                     </div>
                 </div>
             </div>
