1697dc4a0c9eba602d4b1dc1cf6683c31629061f
Henrique-Fiorotti <hberdoldifiorotti@gmail.com>
2026-03-17 09:21:49 -0300

    Fixing some consistency style problems

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -4,10 +4,10 @@ export default function notfound(){
             <div className="w-full h-svh bg-white flex justify-center items-center">
                 <div className="flex flex-col justify-center items-center gap-y-5">
                     <img src="/hero_404.svg" alt="" className="mr-16" />
-                    <h1 className="text-6xl text-[#5e17eb] font-light font-[family:var(--font-poppins)]">404</h1>
+                    <h1 style={{fontFamily: "'Poppins', sans-serif", fontSize: "3.5rem", color: "#5e17eb"}} className="text-7xl">404</h1>
                     <div className="text-center">
-                        <h3 className="text-black">Parece que você se perdeu</h3>
-                        <h3 className="text-black">Voltar para <a href="/" className="text-[#5e17eb] underline">Página inicial</a></h3>
+                        <h3 className="text-black text-[15px]!">Parece que você se perdeu</h3>
+                        <h3 className="text-black text-[15px]!">Voltar para <a href="/" className="text-[#5e17eb]! underline!">Página inicial</a></h3>
                     </div>
                 </div>
             </div>
