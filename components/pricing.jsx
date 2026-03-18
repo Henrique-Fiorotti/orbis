bcac7a77225bb1648854767dc2b092da454d1d92
Henrique-Fiorotti <hberdoldifiorotti@gmail.com>
2026-03-18 08:45:50 -0300

    Changing some details for more consistency

--- components/pricing.jsx
+++ components/pricing.jsx
@@ -4,25 +4,10 @@ import { Separator } from "@/components/ui/separator";
 
 const plans = [
   {
-    name: "Starter",
-    price: 19,
-    description:
-      "Get 20 AI-generated portraits with 2 unique styles and filters.",
-    features: [
-      "5 hours turnaround time",
-      "20 AI portraits",
-      "Choice of 2 styles",
-      "Choice of 2 filters",
-      "2 retouch credits",
-    ],
-    buttonText: "Get 20 portraits in 5 hours",
-  },
-  {
-    name: "Advanced",
-    price: 29,
+    name: "Empresarial",
     isRecommended: true,
     description:
-      "Get 50 AI-generated portraits with 5 unique styles and filters.",
+      "Para empresas profissionais em larga escala.",
     features: [
       "3 hours turnaround time",
       "50 AI portraits",
@@ -30,14 +15,13 @@ const plans = [
       "Choice of 5 filters",
       "5 retouch credits",
     ],
-    buttonText: "Get 50 portraits in 3 hours",
+    buttonText: "Entre em contato",
     isPopular: true,
   },
   {
-    name: "Premium",
-    price: 49,
+    name: "Equipes",
     description:
-      "Get 100 AI-generated portraits with 10 unique styles and filters.",
+      "Para equipes de 10 a 150 profissionais em larga escala.",
     features: [
       "1-hour turnaround time",
       "100 AI portraits",
@@ -45,40 +29,42 @@ const plans = [
       "Choice of 10 filters",
       "10 retouch credits",
     ],
-    buttonText: "Get 100 portraits in 1 hour",
-  },
+    buttonText: "Entre em contato",
+  }
 ];
 
 const Pricing = () => {
   return (
     <div
-      className="flex min-h-screen flex-col items-center justify-center px-6 py-12">
-      <h2 className="text-center font-semibold text-5xl tracking-[-0.03em]">
-        Our Plans
+      className="flex min-h-screen flex-col items-center justify-center px-15! py-12">
+      <h2 className="text-center font-semibold text-4xl! tracking-[-0.03em]">
+        Nossos <span className="text-[#5E17EB]">planos</span>
       </h2>
       <p className="mt-3 text-center text-muted-foreground text-xl">
-        Choose the plan that fits your needs and get started today
+        Escolha o plano que se adeque às suas necessidades <br /> e comece hoje a prever seu problemas.
       </p>
       <div
-        className="mx-auto mt-12 grid max-w-(--breakpoint-lg) grid-cols-1 gap-8 sm:mt-16 lg:grid-cols-3">
+        className="mx-auto mt-8! grid max-w-(--breakpoint-lg) grid-cols-1 gap-8 sm:mt-16 lg:grid-cols-2">
         {plans.map((plan) => (
-          <div className="rounded-lg border p-6" key={plan.name}>
+          <div className="rounded-lg border p-6"
+          variant={plan.isPopular ? "default" : "outline"}
+          key={plan.name}>
             <h3 className="font-medium text-lg">{plan.name}</h3>
             <p className="mt-2 font-semibold text-4xl">${plan.price}</p>
             <p className="mt-4 font-medium text-muted-foreground">
               {plan.description}
             </p>
             <Separator className="my-4" />
-            <ul className="space-y-2">
+            <ul className="space-y-1 pl-1!">
               {plan.features.map((feature) => (
                 <li className="flex items-start gap-2" key={feature}>
-                  <CircleCheck className="mt-1 h-4 w-4 text-green-600" />{" "}
+                  <CircleCheck className="mt-1 h-4 w-4 text-[#5E17EB]" />{" "}
                   {feature}
                 </li>
               ))}
             </ul>
             <Button
-              className="mt-6 w-full"
+              className="mt-6 h-12 w-full bg-[#5E17EB] rounded-[10px]! hover:bg-[#5013ca]! text-[#ffffff]!"
               size="lg"
               variant={plan.isPopular ? "default" : "outline"}>
               {plan.buttonText}
