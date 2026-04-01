cf5bff6ac6baa0f82d450bab0d63e6d0db95a985
henrique-fiorotti <hberdoldifiorotti@gmail.com>
2026-04-01 17:23:52 -0300

    fix: improve layout consistency in SiteHeader component

--- components/site-header.jsx
+++ components/site-header.jsx
@@ -1,12 +1,23 @@
 import { Separator } from "@/components/ui/separator"
 import { SidebarTrigger } from "@/components/ui/sidebar"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
 
 export function SiteHeader() {
   return (
     <header
       className="flex h-[90px] shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
-        <SidebarTrigger className="-ml-1" />
+        <Tooltip>
+          <TooltipTrigger><SidebarTrigger className="-ml-1" /></TooltipTrigger>
+          <TooltipContent>
+            <p className="m-0!">Expandir/Contrair Sidebar</p>
+          </TooltipContent>
+        </Tooltip>
+
         <Separator orientation="vertical" className="mx-2 data-[orientation=vertical]:h-auto" />
         <div className="">
           <h1 className="text-gray-500! text-base text-[13pt]! font-medium m-0!">Dashboard Orbis</h1>
