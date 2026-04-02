d8b6e203a669ddfdff2b575c81960a1a36f2ffff
gabrielhub123 <gabriel.pllegleg@gmail.com>
2026-04-02 16:32:43 -0300

    Style: Redirecionando para home quando tela for atualizada

--- app/(public)/login/page.jsx
+++ app/(public)/login/page.jsx
@@ -1,6 +1,8 @@
 import LoginCard from "@/components/LoginCard/page";
+import {redirect} from "next/navigation";
 
 export default function LoginPage() {
+  redirect("/");
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50">
       <LoginCard />
