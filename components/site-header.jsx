bfaeae7da596ddb5bb297b8b4862c488676cae06
Gabriel Lourencetti <gabriel.pllegleg@gmail.com>
2026-04-16 22:29:37 -0300

    fix: tirando erros do dashboard

--- components/site-header.jsx
+++ components/site-header.jsx
@@ -127,8 +127,9 @@ export function SiteHeader() {
   return (
     <header className="flex h-[90px] shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
+
         <Tooltip>
-          <TooltipTrigger>
+          <TooltipTrigger asChild>
             <SidebarTrigger className="dark:text-white dark:hover:bg-gray-200/10!" />
           </TooltipTrigger>
           <TooltipContent>
@@ -140,6 +141,7 @@ export function SiteHeader() {
           orientation="vertical"
           className="mx-2 data-[orientation=vertical]:h-auto"
         />
+
         <div>
           <h1 className="text-gray-500! text-[10pt]! m-0! dark:text-gray-300!">
             Dashboard Orbis
@@ -152,8 +154,10 @@ export function SiteHeader() {
         <div className="flex-1" />
 
         <div className="flex items-center gap-1">
+
+          {/* Tour */}
           <Tooltip>
-            <TooltipTrigger>
+            <TooltipTrigger asChild>
               <Button
                 variant="ghost"
                 size="icon-sm"
@@ -169,8 +173,9 @@ export function SiteHeader() {
             </TooltipContent>
           </Tooltip>
 
+          {/* Tema */}
           <Tooltip>
-            <TooltipTrigger>
+            <TooltipTrigger asChild>
               <Button
                 variant="ghost"
                 size="icon-sm"
@@ -193,9 +198,10 @@ export function SiteHeader() {
             </TooltipContent>
           </Tooltip>
 
+          {/* Notificações */}
           <div className="relative" ref={panelRef}>
             <Tooltip>
-              <TooltipTrigger>
+              <TooltipTrigger asChild>
                 <Button
                   variant="ghost"
                   size="icon-sm"
@@ -309,6 +315,7 @@ export function SiteHeader() {
               </div>
             )}
           </div>
+
         </div>
       </div>
     </header>
