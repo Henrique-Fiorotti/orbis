54d01acecfbf852cec86d5e56328236fa557c541
henrique-fiorotti <hberdoldifiorotti@gmail.com>
2026-04-20 11:06:28 -0300

    Fix dashboard tour scrolling and layout

--- components/SmootScroll.jsx
+++ components/SmootScroll.jsx
@@ -3,6 +3,8 @@
 import { useEffect } from 'react'
 import Lenis from 'lenis'
 
+const TOUR_SCROLL_LOCK_CHANGE = 'tour.scrollLockChange'
+
 export default function SmoothScroll({ children }) {
   useEffect(() => {
     const lenis = new Lenis({
@@ -11,14 +13,25 @@ export default function SmoothScroll({ children }) {
       smoothWheel: true,
     })
 
+    function handleScrollLockChange(event) {
+      if (event.detail?.locked) {
+        lenis.stop()
+        return
+      }
+
+      lenis.start()
+    }
+
     function raf(time) {
       lenis.raf(time)
       requestAnimationFrame(raf)
     }
 
+    window.addEventListener(TOUR_SCROLL_LOCK_CHANGE, handleScrollLockChange)
     requestAnimationFrame(raf)
 
     return () => {
+      window.removeEventListener(TOUR_SCROLL_LOCK_CHANGE, handleScrollLockChange)
       lenis.destroy()
     }
   }, [])
